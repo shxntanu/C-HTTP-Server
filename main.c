823a963da987ef89fff07de6349ecb1219db68d0
Shantanu <shantanuwable2003@gmail.com>
2024-01-29 00:37:30 +0530

    Fix issue with text rendering

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 
 void launch(struct Server *server) {
     char buffer[30000];
+    int address_length = sizeof(server->address);
+    int new_socket;
+    const char *hello = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Simple HTTP Server</h1></body></html>";
+
     while(1) {
+        new_socket = accept(server->socket, (struct sockaddr*)&server->address, (socklen_t *)&address_length);
         printf("Waiting for connection...\n");
-        int address_length = sizeof(server->address);
-        const int new_socket = accept(server->socket, (struct sockaddr*)&server->address, (socklen_t *)&address_length);
         read(new_socket, buffer, 30000);
         printf("%s\n", buffer);
-        const char *hello = "HTTP/1.1 200 OK\nContent-Type: text/html\n<html>\n<head>\n<title>Example Server</title>\n</head>\n<body>\n<h1>This is an example server.</h1>\n<p>This server is running on port 80.</p>\n</body>\n</html>";
         write(new_socket, hello, strlen(hello));
         close(new_socket);
     }
