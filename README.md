7f19aaedcce108d86825fba5a68c99f91b094bfe
Shantanu <shantanuwable2003@gmail.com>
2024-01-29 07:45:18 +0530

    Edit folder structure zsh:1: command not found: q

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Simple HTTP Web Server in C
+
+This is a simple HTTP Server written in C. It was written as a fun project to learn more about how HTTP works, but in a low level language. It is not meant to be used in production, but it is a good starting point for anyone who wants to learn more about HTTP and how web servers work.
+
+## File Structure
+
+```
+.
+├── .idea
+├── cmake-build-debug
+├── CMakeLists.txt
+├── README.md
+├── main.c
+├── src
+│   ├── server.c
+│   ├── server.h
+
+```
+
+## Building
+
+To build the project, you will need to have CMake installed. Once you have CMake installed, you can run the following commands to build the project:
+
+```
+mkdir build
+cd build
+cmake ..
+make
+```
--- main.c
+++ main.c
@@ -1,25 +1,31 @@
+#include "src/Server.h"
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
-#include "Server.h"
+#include <unistd.h>
 
 void launch(struct Server *server) {
-    char buffer[30000];
-    int address_length = sizeof(server->address);
-    int new_socket;
-    const char *hello = "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Simple HTTP Server</h1></body></html>";
+  char buffer[30000];
+  int address_length = sizeof(server->address);
+  int new_socket;
+  const char *hello =
+      "HTTP/1.1 200 OK\nDate: Mon, 27 Jul 2009 12:28:53 GMT\nServer: "
+      "Apache/2.2.14 (Win32)\nLast-Modified: Wed, 22 Jul 2009 19:15:56 "
+      "GMT\nContent-Length: 88\nContent-Type: text/html\nConnection: "
+      "Closed\n\n<html><body><h1>Simple HTTP Server</h1></body></html>";
 
-    while(1) {
-        new_socket = accept(server->socket, (struct sockaddr*)&server->address, (socklen_t *)&address_length);
-        printf("Waiting for connection...\n");
-        read(new_socket, buffer, 30000);
-        printf("%s\n", buffer);
-        write(new_socket, hello, strlen(hello));
-        close(new_socket);
-    }
+  while (1) {
+    new_socket = accept(server->socket, (struct sockaddr *)&server->address,
+                        (socklen_t *)&address_length);
+    printf("Waiting for connection...\n");
+    read(new_socket, buffer, 30000);
+    printf("%s\n", buffer);
+    write(new_socket, hello, strlen(hello));
+    close(new_socket);
+  }
 }
 
 int main() {
-    struct Server server = server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 80, 10, launch);
-    server.launch(&server);
+  struct Server server =
+      server_constructor(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 80, 10, launch);
+  server.launch(&server);
 }
